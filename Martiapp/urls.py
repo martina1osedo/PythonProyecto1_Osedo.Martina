e02ce32a09532f74e21dda49c8671347d2ada206
Martina Osedo <osedomartina3@gmail.com>
2025-03-22 07:04:49 -0300

    firts commit

--- Martiapp/urls.py
+++ Martiapp/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = "Martiapp"
 
 urlpatterns = [
-    path("post/list", views.index2, name="index2"),
+   # path("index2", views.index2, name="index2"),
+    path("index2", views.PostListView.as_view(), name="post_list"),
+    path("post/create", views.post_create, name="post_create"),
+    
 ]
--- Martiapp/views.py
+++ Martiapp/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.shortcuts import render, redirect
 from .models import Post 
+from .forms import PostForm
 
 
 def index2(request):
-    index2 = Post.objects.all()
+    busqueda = request.GET.get("busqueda", None)
+    if busqueda:
+        index2 = Post.objects.filter(titulo__icontains=busqueda)
+    else:    
+        index2 = Post.objects.all()
     return render(request, 'Martiapp/index2.html', context={"posts": index2})
+
+class PostListView(ListView):
+    model = Post
+    template_name = "Martiapp/index2.html"
+    context_object_name = "posts"
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        busqueda = self.request.GET.get("busqueda", None)
+        if busqueda: 
+            queryset = queryset.filter(titulo_icontains=busqueda)
+            return QuerySet
+                       
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            if request.user.is_authenticated:
+                post.autor = request.user
+                post.save()
+                return redirect("Martiapp:index2")
+            else: 
+                form.add_error(None, "Debes estar logueado para crear una publicacion")
+                    
+    else:
+        form = PostForm()    
+    return render(request, "Martiapp/post_create.html", context={"form": form})
